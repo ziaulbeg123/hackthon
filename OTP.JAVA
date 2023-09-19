9f512a4368d4afd61b3ec12ee905b9cbe7dd0ef8
mohd ziaul beg <116621976+ziaulbeg123@users.noreply.github.com>
2023-09-19 15:48:41 +0530

    Add files via upload

---
+++ OTP.JAVA
@@ -0,0 +1,17 @@
+import java.util.*;  
+class OneTime{  
+public static String generateOTP(int length) {  
+    String numbers = "0123456789";  
+    Random rndm_method = new Random();  
+    char[] otp = new char[length];  
+    for (int i = 0; i < length; i++) {  
+        otp[i] = numbers.charAt(rndm_method.nextInt(numbers.length()));  
+    }  
+    return new String(otp);  
+}  
+public static void main(String[] args) {  
+    int length = 6;  
+    String otp = generateOTP(length);  
+    System.out.println("Your One Time Password is: " + otp);  
+}  
+}  
